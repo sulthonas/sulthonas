b8d545a238722884ed9538f7cffe2f79b314e89a
sulthonas <130933192+sulthonas@users.noreply.github.com>
2024-03-25 10:35:06 +0700

    Create main.py

---
+++ Random Menu Makan/main.py
@@ -0,0 +1,13 @@
+import random
+random = random.randrange(1, 5)
+
+if random == 1:
+  print("hari ini buka puasa makan soto")
+if random == 2:
+  print("hari ini buka puasa makan nasi padang")
+if random == 3:
+  print("hari ini buka puasa makan ikan bakar")
+if random == 4:
+  print("hari ini buka puasa makan sate kambing")
+if random == 5:
+  print("hari ini buka puasa makan bakso")

39705448a33076cf2534674eaca1dd8403cc491b
sulthonas <130933192+sulthonas@users.noreply.github.com>
2024-03-25 10:31:02 +0700

    Add files via upload

---
+++ Kalkulator Menghitung Persegi Panjangt/main.py
@@ -0,0 +1,6 @@
+panjang = int(input("masukkan panjang persegi panjang:"))
+lebar = int(input("masukkan lebar persegi panjang:"))
+
+luas = panjang * lebar
+
+print("luas persegi panjang dengan panjang", panjang, "dan lebar", lebar, "adalah", luas)
